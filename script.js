f9095955e5488b8b42e8323536b9585f0bf55ce0
TypingAndy <andreas.georg@outlook.com>
2024-11-02 12:42:43 +0100

    summary bugfix

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let sortedUsers = [];
 let allUserInitials = [];
 let userColorsPreset = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"];
 let userColors = [];
-
 let policyAccepted = false;
 let passwordMatch = false;
 let chosenCategory = "";
@@ -36,9 +35,6 @@ let awaitFeedbackTasks = [];
 let currentDraggedElementID;
 let currentNumberedID = "";
 
-//summaryGlobalArrays
-let allUrgentTasksCount = 0;
-let allUrgentTasks = [];
 
 //oftenUsedGlobalArrays
 let currentDate = "";
