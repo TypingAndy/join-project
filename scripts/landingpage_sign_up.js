9eb5dbf68e56dbc0d188842654d08d8a01ddcfe4
daberan <mail@davidberan.com>
2024-11-25 14:38:59 +0100

    ojoj

--- scripts/landingpage_sign_up.js
+++ scripts/landingpage_sign_up.js
@@ -9,20 +9,24 @@ function clearSignUpInputField() {
   confirmPasswordInput.value = "";
 }
 
+function clickAcceptPolicy() {
+  acceptPrivacyPolicyCheck();
+  validateSignUp();
+}
+
 function validateSignUp() {
   let button = document.getElementById("signUpButton");
-  let nameValue = document.getElementById("signUpNameInput").value.trim();
-  let emailValue = document.getElementById("signUpMailInput").value.trim();
+  let nameValue = document.getElementById("signUpNameInput").value;
+  let emailValue = document.getElementById("signUpMailInput").value;
+
   if (policyAccepted && passwordMatch && nameValue && emailValue) {
     button.style.opacity = "1";
-    button.disabled = false;
     button.setAttribute("onclick", "handleSignUpClick()");
-    button.style = "cursor: pointer;";
+    button.style.cursor = "pointer";
   } else {
     button.style.opacity = "0.1";
-    button.disabled = true;
     button.removeAttribute("onclick");
-    button.style = "cursor: auto;";
+    button.style.cursor = "default";
   }
 }
 
